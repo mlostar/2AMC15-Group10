433c80db27d99ff532d4e3ca4b869e0706f9f127
Yi He <yihezhu@gmail.com>
2022-05-07 21:23:56 +0200

    reformatted file

--- Discrete-Simulations/robot_configs/policy_iteration_robot.py
+++ Discrete-Simulations/robot_configs/policy_iteration_robot.py
@@ -4,12 +4,13 @@ import numpy as np
 DISCOUNT_FACTOR = 0.9
 THETA = 0.01
 
+
 def robot_epoch(robot):
     # Initialization
     possible_moves = list(robot.dirs.values())
 
     V = np.zeros_like(robot.grid.cells)  # initialize V
-    policy = 1/4 * np.ones((robot.grid.n_cols, robot.grid.n_rows, 4)) # initialize equiprobably policy
+    policy = 1 / 4 * np.ones((robot.grid.n_cols, robot.grid.n_rows, 4))  # initialize equal probability policy
 
     i = 0
     policy_stable = False
@@ -18,30 +19,30 @@ def robot_epoch(robot):
         # Policy evaluation
         policy_evaluation(robot, V, policy)
 
-        #Policy improvement
-        policy_stable = policy_improvment(robot, V, policy)
+        # Policy improvement
+        policy_stable = policy_improvement(robot, V, policy)
 
     # print(f"Policy iteration convergence: {i} iterations")
 
-    action_values = [] # not sure about this calc
+    action_values = []  # not sure about this calc
     (x, y) = robot.pos
-    for p_move in policy[x,y,:]:
+    for p_move in policy[x, y, :]:
         action_values.append(p_move)
 
     # Find the best move by finding the action with highest value for this state
     move = possible_moves[action_values.index(max(action_values))]
     # Find out how we should orient ourselves:
     new_orient = get_orientation_by_move(robot, move=move)
-    
+
     print(f'\nRobot pos: {robot.pos}')
     print(f'Action values: {action_values}')
     for move in possible_moves:
-        print(f'V({add_coords(robot.pos , move)}): {V[add_coords(robot.pos , move)]}')
-        print(f'Reward({add_coords(robot.pos,move)}): {get_reward(robot, add_coords(robot.pos,move))}')
+        print(f'V({add_coords(robot.pos, move)}): {V[add_coords(robot.pos, move)]}')
+        print(f'Reward({add_coords(robot.pos, move)}): {get_reward(robot, add_coords(robot.pos, move))}')
 
     move_robot(robot, new_orient)
 
-        
+
 def policy_evaluation(robot, V, policy):
     i = 0
     while True:
@@ -51,17 +52,17 @@ def policy_evaluation(robot, V, policy):
         n_rows = robot.grid.n_rows
         n_cols = robot.grid.n_cols
 
-        for x in range(1, n_cols-1):
-            for y in range(1, n_rows-1):
+        for x in range(1, n_cols - 1):
+            for y in range(1, n_rows - 1):
                 # Robot cant visit negative tiles
                 if robot.grid.cells[x, y] < 0:
                     continue
 
                 v = 0
-                v += policy[x,y,0] * (get_reward(robot,(x,y-1))+DISCOUNT_FACTOR*V[x, y-1]) # north
-                v += policy[x,y,1] * (get_reward(robot,(x+1,y))+DISCOUNT_FACTOR*V[x+1, y]) # east
-                v += policy[x,y,2] * (get_reward(robot,(x,y+1))+DISCOUNT_FACTOR*V[x, y+1]) # south
-                v += policy[x,y,3] * (get_reward(robot,(x-1,y))+DISCOUNT_FACTOR*V[x-1, y]) # west
+                v += policy[x, y, 0] * (get_reward(robot, (x, y - 1)) + DISCOUNT_FACTOR * V[x, y - 1])  # north
+                v += policy[x, y, 1] * (get_reward(robot, (x + 1, y)) + DISCOUNT_FACTOR * V[x + 1, y])  # east
+                v += policy[x, y, 2] * (get_reward(robot, (x, y + 1)) + DISCOUNT_FACTOR * V[x, y + 1])  # south
+                v += policy[x, y, 3] * (get_reward(robot, (x - 1, y)) + DISCOUNT_FACTOR * V[x - 1, y])  # west
 
                 delta = max(delta, np.abs(V[x][y] - v))
 
@@ -71,51 +72,54 @@ def policy_evaluation(robot, V, policy):
             break
     # print(f"Policy evaluation convergence: {i} iterations")
 
-def policy_improvment(robot, V, policy):
-    policy_stable = True # assume stable at first
+
+def policy_improvement(robot, V, policy):
+    policy_stable = True  # assume stable at first
 
     n_rows = robot.grid.n_rows
     n_cols = robot.grid.n_cols
 
-    for x in range(1, n_cols-1):
-        for y in range(1, n_rows-1):
-            old_policy = policy[x,y,:].copy()
+    for x in range(1, n_cols - 1):
+        for y in range(1, n_rows - 1):
+            old_policy = policy[x, y, :].copy()
             best_actions = []
             max_v = -math.inf
-            
+
             # Check best actions
-            action_values = [get_reward(robot,(x,y-1))+DISCOUNT_FACTOR*V[x,y-1],
-                            get_reward(robot,(x+1,y))+DISCOUNT_FACTOR*V[x+1,y],
-                            get_reward(robot,(x,y+1))+DISCOUNT_FACTOR*V[x,y+1],
-                            get_reward(robot,(x-1,y))+DISCOUNT_FACTOR*V[x-1,y]]
+            action_values = [get_reward(robot, (x, y - 1)) + DISCOUNT_FACTOR * V[x, y - 1],
+                             get_reward(robot, (x + 1, y)) + DISCOUNT_FACTOR * V[x + 1, y],
+                             get_reward(robot, (x, y + 1)) + DISCOUNT_FACTOR * V[x, y + 1],
+                             get_reward(robot, (x - 1, y)) + DISCOUNT_FACTOR * V[x - 1, y]]
 
             for action, value in enumerate(action_values):
                 if value > max_v:
                     max_v = value
                     best_actions = [action]
-                elif value == max_v: # handle possible same action value
+                elif value == max_v:  # handle possible same action value
                     best_actions.append(action)
 
             # Create new policy
-            prob = 1/len(best_actions)
+            prob = 1 / len(best_actions)
             for action in range(len(action_values)):
                 if action in best_actions:
-                    policy[x,y,action] = prob
+                    policy[x, y, action] = prob
                 else:
-                    policy[x,y,action] = 0
+                    policy[x, y, action] = 0
 
             # Check if policy changed
-            if not (old_policy == policy[x,y,:]).all():
+            if not (old_policy == policy[x, y, :]).all():
                 policy_stable = False
 
     return policy_stable
 
+
 def get_reward(robot, pos):
     if robot.pos == pos:
         return 0
-    if robot.grid.cells[pos[0],pos[1]] == 3:
+    if robot.grid.cells[pos[0], pos[1]] == 3:
         return -3
-    return robot.grid.cells[pos[0],pos[1]]
+    return robot.grid.cells[pos[0], pos[1]]
+
 
 def move_robot(robot, direction):
     # Orient ourselves towards the dirty tile:
@@ -126,9 +130,11 @@ def move_robot(robot, direction):
     # Move:
     robot.move()
 
+
 # Returns one of the directions (e.g. 'n') given the move
 def get_orientation_by_move(robot, move):
     return list(robot.dirs.keys())[list(robot.dirs.values()).index(move)]
 
+
 def add_coords(c1, c2):
-    return (c1[0]+c2[0], c1[1]+c2[1])
+    return (c1[0] + c2[0], c1[1] + c2[1])
