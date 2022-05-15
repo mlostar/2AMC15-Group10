5ecf29a15b3186b0404ba0fbd01173964d4ac8d1
mlostar <lostarmert@gmail.com>
2022-05-15 09:24:57 +0200

    Initial implementation of on policy monte carlo

---
+++ Discrete-Simulations/robot_configs/monte_carlo_robot.py
@@ -0,0 +1,182 @@
+import copy
+
+import numpy as np
+from numpy.random import choice
+
+epsilon = 0.35
+gamma = 0.75
+
+
+def robot_epoch(robot):
+    # (move_orientation, (move_increment)) pairs
+    move_pairs = list(robot.dirs.items())
+    # no of rows
+    n_rows = robot.grid.n_rows
+    # no of cols
+    n_cols = robot.grid.n_cols
+    # Initialization of e-soft policy, Q and returns
+    Q = np.zeros((n_cols, n_rows, 4)).tolist()
+    policy = ((np.ones((n_cols, n_rows, 4)) * epsilon) / 4).tolist()
+    global_returns = [[[[] for i in range(4)] for row in range(n_rows)] for col in range(n_cols)]
+
+    # Hack to never explore walls/obstacles in the first iteration
+    # Get rewards
+    rewards = grid_to_rewards(robot)
+    for x in range(1, n_cols - 1):
+        for y in range(1, n_rows - 1):
+            for action_id in range(4):
+                move_orientation, _ = move_pairs[action_id]
+                (next_x, next_y) = coordinate_finder((x, y), move_orientation)
+                # Make sure to never explore moves into walls/obstacles
+                if rewards[next_x][next_y] == -1:
+                    policy[x][y][action_id] = 0
+
+    # Repeat till values converge
+    iter = 0
+    while True:
+        # Get rewards
+        rewards = grid_to_rewards(robot)
+        ep_history, ep_returns = generate_episode(policy, robot)
+        unique_pairs = list(dict.fromkeys(ep_history))
+
+        # Update Q
+        for pair in unique_pairs:
+            (x, y, move_orientation) = pair
+            move_idx = list(robot.dirs.keys()).index(move_orientation)
+            pair_idx = ep_history.index(pair)
+            pair_return = ep_returns[pair_idx]
+            global_returns[x][y][move_idx].append(pair_return)
+            # Calculate average
+            temp = global_returns[x][y][move_idx]
+            avg = sum(temp) / len(temp)
+            Q[x][y][move_idx] = avg
+        prev_policy = copy.deepcopy(policy)
+        # Update policy
+        for x in range(1, n_cols - 1):
+            for y in range(1, n_rows - 1):
+                # Action that maximizes Q(s,a)
+                max_action_idx = Q[x][y].index(max(Q[x][y]))
+                for action_id in range(4):
+                    move_orientation, _ = move_pairs[action_id]
+                    (next_x, next_y) = coordinate_finder((x, y), move_orientation)
+                    # Make sure to never explore moves into walls/obstacles
+                    if rewards[next_x][next_y] == -1:
+                        policy[x][y][action_id] = 0
+                    elif max_action_idx == action_id:
+                        policy[x][y][max_action_idx] = 1 - epsilon + epsilon / 4
+                    else:
+                        policy[x][y][action_id] = epsilon / 4
+        iter += 1
+        if np.array_equal(prev_policy, policy) or iter == 200:
+            print("Iters until convergence: ", iter)
+            break
+    # Find next move based on converged policy
+    current_pos = robot.pos
+    next_move_id = policy[current_pos[0]][current_pos[1]].index(max(policy[current_pos[0]][current_pos[1]]))
+    next_orientation = move_pairs[next_move_id][0]
+    # Move
+    move_robot(robot, next_orientation)
+
+
+def generate_episode(policy, robot):
+    # (move_orientation, (move_increment)) pairs
+    move_pairs = list(robot.dirs.items())
+    current_pos = robot.pos
+    # (position,action_orientation) pairs in order
+    history = []
+    # Stores returns of (position,action_orientation) pairs
+    episode_returns = []
+    is_episode_terminated = False
+    rewards = grid_to_rewards(robot)
+    original_rewards = copy.deepcopy(rewards)
+    # Run episode
+    while not is_episode_terminated:
+        # Find best move from policy
+        chosen_move_idx = get_move_from_policy(policy, current_pos)
+        move_orientation, move_increment = move_pairs[chosen_move_idx]
+        next_pos = coordinate_finder(current_pos, move_orientation)
+        reward = rewards[next_pos[0]][next_pos[1]]
+        original_reward = original_rewards[next_pos[0]][next_pos[1]]
+        # Ignore moves into walls/obstacles
+        if original_reward != -1:
+            # Add the move to history of moves
+            history.append((current_pos[0], current_pos[1], move_orientation))
+            # Add reward of the move to returns of all previous moves and append
+            length = len(episode_returns)
+            episode_returns = [episode_returns[i] + pow(gamma, length - i) * reward for i in range(0, length)]
+            episode_returns.append(reward)
+            # Set current position to the next position if move was not into a wall/obstacle
+            current_pos = next_pos
+            # if rewards[current_pos[0]][current_pos[1]] == 1:
+            rewards[current_pos[0]][current_pos[1]] = -0.1
+            # else:
+            #    rewards[current_pos[0]][current_pos[1]] = -0.5
+        # Terminate the episode once the robot makes 70 moves
+        # or if the robot steps on a death cell
+        if len(episode_returns) >= 80 or original_rewards[current_pos[0], current_pos[1]] == -10:
+            is_episode_terminated = True
+    return history, episode_returns
+
+
+def get_move_from_policy(policy, current_pos):
+    x = current_pos[0]
+    y = current_pos[1]
+    indices = [0, 1, 2, 3]  # move indices
+    # Temporarily normalize policy probabilities so that they add up to 1
+    # For compatibility with np.choice
+    p = np.array(policy[x][y])
+    p /= p.sum()
+    # Choose next move based on policy probabilities
+    move_idx = choice(indices, p=p)
+    return move_idx
+
+
+def grid_to_rewards(robot):
+    """
+    Returns a reward grid based on the input robot.
+    """
+    temp_grid = copy.deepcopy(robot.grid.cells)
+    temp_grid[robot.pos] = 0
+
+    values = {-2: -1.,
+              -1: -1.,
+              0: -0.1,
+              1: 5.,
+              2: 10.,
+              3: -10.}
+    return np.vectorize(values.__getitem__)(temp_grid)
+
+
+def add_coords(c1, c2):
+    """
+    Return the two input coords added together.
+    """
+    return (c1[0] + c2[0], c1[1] + c2[1])
+
+
+def coordinate_finder(pos, direction):
+    """
+    Given an initial position and move direction calculate the next position
+    calculate the next position based on the given current position and move direction
+    """
+    if direction == 'n':
+        return (pos[0], pos[1] - 1)
+    elif direction == 's':
+        return (pos[0], pos[1] + 1)
+    elif direction == 'e':
+        return (pos[0] + 1, pos[1])
+    elif direction == 'w':
+        return (pos[0] - 1, pos[1])
+
+
+def move_robot(robot, direction):
+    """
+    Rotates the robot into the given direction and calls the move function.
+    """
+    # Orient ourselves towards the dirty tile:
+    while direction != robot.orientation:
+        # If we don't have the wanted orientation, rotate clockwise until we do:
+        # print('Rotating right once.')
+        robot.rotate('r')
+    # Move:
+    robot.move()
