d3f924ec0be61d50b9c5d0cf40317b7ab9169902
J.K.B. Chan <j.k.b.chan@student.tue.nl>
2022-06-18 10:59:18 +0200

    Add a3c

---
+++ final/demos/a3c.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import numpy as np
+from ray.rllib.agents.a3c import A3CTrainer
+
+from final.env.env import FloorCleaning
+from final.env.robot import Robot
+from final.evaluation import get_cleaning_efficiency
+from final.utils.parsing import parse_config
+
+parent_path = Path(".").resolve().parent
+grid = parse_config(Path(".").parent/"assets"/"simple.grid")
+robot = Robot(init_position=(0, 8))
+
+
+trainer = A3CTrainer(env=FloorCleaning, config={"env_config": {"robot": robot, "grid": grid},
+                                                # Learning rate
+                                                "lr": 0.0001,
+                                                # Entropy coefficient
+                                                "entropy_coeff": 0.01})
+
+env = FloorCleaning(dict(robot=robot, grid=grid))
+
+checkpoint_path = None
+for e in range(20):
+    print(trainer.train())
+    #checkpoint_path = trainer.save(checkpoint_dir=parent_path/"checkpoints")
+
+    efficiency = get_cleaning_efficiency(env, lambda o: trainer.compute_single_action(o), max_steps=100)
+    print(f"Epoch: {e} -- Efficiency: {efficiency}")
+
+
+#trainer.restore(checkpoint_path)
+for e in range(10):
+    obs = env.reset()
+    env.render()
+
+    for s in range(1000):
+        move = trainer.compute_action(obs)
+        obs, reward, has_ended, info = env.step(move)
+        env.render()
+        print(f"move: {move/(2*np.pi)*360}, reward: {reward}")
+
+        if has_ended:
+            print(f"Game over: {info['reason']}")
+            break
